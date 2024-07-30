fed35e2bfd69aa96efc93606d528a7dbce1f155c
HyteX <khalil.hytex@gmail.com>
2024-07-30 15:48:55 +0100

    init

---
+++ src/main/java/com/moe/springbootsupabaseauth/SpringBootSupabaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.moe.springbootsupabaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSupabaseAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSupabaseAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moe/springbootsupabaseauth/SpringBootSupabaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moe.springbootsupabaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSupabaseAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
